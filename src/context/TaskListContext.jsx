44c067d8b7cb13960194d65603a9936ce99e67d2
Himanshu Kara <kara_himanshu@yahoo.com>
2021-07-18 23:19:39 +0530

    Edit bug fix

--- src/context/TaskListContext.jsx
+++ src/context/TaskListContext.jsx
@@ -4,32 +4,50 @@ import { v1 as uuid } from "uuid";
 export const TaskListContext = createContext();
 
 const TaskListContextProvider = (props) => {
-  const [tasks, setTasks] = useState([]);
+  const initialState = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  const [tasks, setTasks] = useState(initialState);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const [editItem, setEditItem] = useState(null);
 
+  // Add tasks
   const addTask = (title) => {
-    //Add key:uuid()
-    setTasks([...tasks, { title, key: uuid() }]);
-    //Add
+    setTasks([...tasks, { title, id: uuid() }]);
   };
+
+  // Remove tasks
   const removeTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
+  // Clear tasks
   const clearList = () => {
     setTasks([]);
   };
+
+  // Find task
   const findItem = (id) => {
     const item = tasks.find((task) => task.id === id);
+
     setEditItem(item);
   };
+
+  // Edit task
   const editTask = (title, id) => {
     const newTasks = tasks.map((task) =>
       task.id === id ? { title, id } : task
     );
+
+    console.log(newTasks);
+
     setTasks(newTasks);
     setEditItem(null);
   };
+
   return (
     <TaskListContext.Provider
       value={{
